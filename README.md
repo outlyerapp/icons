00575856968377bcef576cda4d638fd1af725337
Michal Srch <michalsrch@gmail.com>
2014-12-05 00:28:54 +0000

    Initial import.

---
+++ README.md
@@ -0,0 +1,47 @@
+# Dataloop Glyph Icons
+
+A collection of icons for [**Dataloop.IO**](https://www.dataloop.io/).
+
+## Development
+
+### Requirements
+
+- Mac OSX
+- [**Sketch 3**](http://bohemiancoding.com/sketch) and [**Sketch Tools**](http://bohemiancoding.com/sketch/tool/)
+- [**Node.js**](http://nodejs.org/)
+- [**gulp.js**](http://gulpjs.com/)
+
+
+### Install
+
+Install **Gulp** and install **NPM** dev dependencies by:
+
+```bash
+$ sudo npm install -g gulp
+$ npm install
+```
+
+
+### Gulp Tasks
+
+To build _dis_ version run default gulp taks run:
+
+```bash
+$ gulp
+```
+
+or
+
+```bash
+$ gulp build
+```
+
+To start watcher which will automatically rebuild your _dist_ after you save your **Sketch** file run:
+
+```bash
+$ gulp watch
+```
+
+## Other Info
+
+The build system was based on project [**symbols-for-sketch**](https://github.com/cognitom/symbols-for-sketch).
---
+++ gulpfile.js
@@ -0,0 +1,77 @@
+var path = require('path');
+var gulp = require('gulp');
+var less = require('gulp-less');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var sketch = require('gulp-sketch');
+var iconfont = require('gulp-iconfont');
+var minifyCSS = require('gulp-minify-css');
+var gulpFilter = require('gulp-filter');
+var consolidate = require('gulp-consolidate');
+
+var sketchFile = 'glyph-font-14px';
+var fontName = 'DL-Icons';
+var classPrefix = 'di';
+var template = 'fontawesome-style';
+
+gulp.task('build', function () {
+
+    gulp.src('src/' + sketchFile + '.sketch')
+        .pipe(sketch({
+          export: 'artboards',
+          formats: 'svg'
+        }))
+        .pipe(gulp.dest('dist/svg/'))
+        .pipe(iconfont({
+            fontName: fontName
+        }))
+        .on('codepoints', function (codepoints) {
+
+            var options = {
+                glyphs: codepoints,
+                fontName: fontName,
+                fontNameLower: fontName.toLowerCase(),
+                fontPath: '../fonts/',
+                className: classPrefix,
+                pkg: require('./package.json')
+            };
+
+            gulp.src('src/templates/' + template + '/*.less')
+                .pipe(consolidate('lodash', options))
+                .pipe(rename(function (path) {
+                    if (path.basename === 'font') path.basename = options.fontNameLower;
+                }))
+                .pipe(gulp.dest('dist/less/'))
+                .pipe(gulpFilter(['*', '!' + options.fontNameLower + '.less']))
+                .pipe(concat('all.less'))
+                .pipe(less())
+                .pipe(rename({
+                    basename: options.fontNameLower
+                }))
+                .pipe(gulp.dest('dist/css/'))
+                .pipe(minifyCSS({
+                    keepSpecialComments: 1
+                }))
+                .pipe(rename({
+                    suffix: '.min'
+                }))
+                .pipe(gulp.dest('dist/css/'));
+
+            gulp.src('src/templates/' + template + '/sample.html')
+                .pipe(consolidate('lodash', options))
+                .pipe(rename({
+                    basename: 'index'
+                }))
+                .pipe(gulp.dest('dist/'));
+            })
+            .pipe(gulp.dest('dist/fonts/'));
+        });
+
+gulp.task('watch', function () {
+
+    gulp.watch('src/' + sketchFile + '.sketch', {
+        debounceDelay: 3000
+    }, ['build']);
+});
+
+gulp.task('default', ['build']);
